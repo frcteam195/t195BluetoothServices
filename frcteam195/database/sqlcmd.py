ac3e8e18d2590a7ba9837c9dd358b1c6294d1d73
bontempi <chrisbontempi@compuserve.com>
2020-03-06 20:54:58 -0500

    More error handling and reporting.

--- frcteam195/database/sqlcmd.py
+++ frcteam195/database/sqlcmd.py
@@ -1,41 +1,61 @@
 from frcteam195.database import connect
 import logging
 import datetime
+import sys
 
 
 def get(cmd):
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(cmd)
-    columns = [column[0] for column in cursor.description]
-    results = None
-    for row in cursor.fetchall():
-        results = dict(zip(columns, row))
-    conn.close()
-    return results
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(cmd)
+        columns = [column[0] for column in cursor.description]
+        results = None
+        for row in cursor.fetchall():
+            results = dict(zip(columns, row))
+        conn.close()
+        return results
+    except:
+        if conn:
+            conn.close()
+        logging.error("Unexpected error: %s".format(sys.exc_info()[0]))
+    pass
+
 
 def get_list(cmd):
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(cmd)
-    columns = [column[0] for column in cursor.description]
-    results = []
-    for row in cursor.fetchall():
-        results.append(dict(zip(columns, row)))
-    conn.close()
-    return results
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(cmd)
+        columns = [column[0] for column in cursor.description]
+        results = []
+        for row in cursor.fetchall():
+            results.append(dict(zip(columns, row)))
+        conn.close()
+        return results
+    except:
+        if conn:
+            conn.close()
+        logging.error("Unexpected error: %s".format(sys.exc_info()[0]))
+        pass
+
 
 def put(cmd):
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(cmd)
-    conn.commit()
-    if cursor.rowcount < 1:
-        conn.close()
-        logging.error(str(datetime.datetime.now()) + " No records were updated: cmd={0}".format(cmd))
-        return("failure")
-    else:
-        conn.close()
-        logging.error(str(datetime.datetime.now()) + " {0} record(s) updated".format(cursor.rowcount))
-        return("success")
-
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(cmd)
+        conn.commit()
+        if cursor.rowcount < 1:
+            conn.close()
+            logging.error(str(datetime.datetime.now()) + " No records were updated: cmd={0}".format(cmd))
+            return ("failure")
+        else:
+            conn.close()
+            logging.error(str(datetime.datetime.now()) + " {0} record(s) updated".format(cursor.rowcount))
+            return ("success")
+    except:
+        if conn:
+            conn.close()
+        logging.error("Unexpected error: %s".format(sys.exc_info()[0]))
+        pass
