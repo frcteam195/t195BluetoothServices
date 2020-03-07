718b7d215e0a951f47af0eb2b0770c5c1d6e65a3
bontempi <chrisbontempi@compuserve.com>
2020-03-07 10:26:26 -0500

    Handle non-numerics in SQL Update statements.

--- frcteam195/database/WordCloud.py
+++ frcteam195/database/WordCloud.py
@@ -5,3 +5,18 @@ def get(eventId):
     results = sqlcmd.get_list("SELECT a.AllianceStationID AllianceStationID, b.* FROM MatchScouting a, WordCloud b " \
                    "WHERE a.EventID = {} and a.MatchID = b.MatchID ORDER BY MatchID, AllianceStationID".format(eventId))
     return results
+
+def put(payload):
+    cmd_skeleton = 'INSERT INTO WordID ({}) VALUES ({})'
+    col_list = []
+    val_list = []
+    for k,v in payload.items():
+        if isinstance(str, v):
+            val_list.append("'{}'".format(v))
+        else:
+            val_list.append("{}".format(v))
+        col_list.append(k)
+    col_str = ','.join(col_list)
+    val_str = ','.join(val_list)
+    cmd = cmd_skeleton.format(col_str, val_str)
+    return sqlcmd.put(cmd)
